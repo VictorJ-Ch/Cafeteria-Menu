c2399afc5bea578331eb1ddce0dd22fc53b1cdf3
Kaseem <chavarria.martinez.victor101@gmail.com>
2023-11-21 00:56:08 -0600

    Intento con Firebase 1
    No pude ni iniciar sesión aquí, pero si que se puede con laravel, pero lo dejo aqui, pq yame estrese.

---
+++ docs/js/modals.js
@@ -0,0 +1,4 @@
+document.getElementById('signUpLink').addEventListener('click', function () {
+    var myModal = new bootstrap.Modal(document.getElementById('signUpModal'));
+    myModal.show();
+});
