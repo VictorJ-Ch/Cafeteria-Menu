817ce5b17e38225f31c28d971b3f4345894dec92
3 <samuelgtz025@gmail.com>
2023-11-25 16:13:20 -0600

    pantalla pastas

---
+++ docs/js/indexPastas.js
@@ -0,0 +1,17 @@
+const contenedorTarjetas = document.getElementById("productos-container");
+
+/** Crea las tarjetas de productos teniendo en cuenta la lista en bicicletas.js */
+function crearTarjetasProductosInicio(productos){
+  productos.forEach(producto => {
+    const nuevaPasta = document.createElement("div");
+    nuevaPasta.classList = "tarjeta-producto"
+    nuevaPasta.innerHTML = `
+    <img src="./img/productos/${producto.id}.webp" alt="Pasta 1">
+    <h3>${producto.nombre}</h3>
+    <p class="precio">$${producto.precio}</p>
+    <button>Agregar al carrito</button>`
+    contenedorTarjetas.appendChild(nuevaPasta);
+    nuevaPasta.getElementsByTagName("button")[0].addEventListener("click",() => agregarAlCarrito(producto))
+  });
+}
+crearTarjetasProductosInicio(pastas);
