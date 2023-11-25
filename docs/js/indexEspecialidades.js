ecafec69aa76e45bb5cc78b1da6a0f0fe80aa810
3 <samuelgtz025@gmail.com>
2023-11-25 16:59:12 -0600

    pantalla especialidades

---
+++ docs/js/indexEspecialidades.js
@@ -0,0 +1,17 @@
+const contenedorTarjetas = document.getElementById("productos-container");
+
+/** Crea las tarjetas de productos teniendo en cuenta la lista en bicicletas.js */
+function crearTarjetasProductosInicio(productos){
+  productos.forEach(producto => {
+    const nuevaEspecial = document.createElement("div");
+    nuevaEspecial.classList = "tarjeta-producto"
+    nuevaEspecial.innerHTML = `
+    <img src="./img/productos/${producto.id}.webp" alt="Especial 1">
+    <h3>${producto.nombre}</h3>
+    <p class="precio">$${producto.precio}</p>
+    <button>Agregar al carrito</button>`
+    contenedorTarjetas.appendChild(nuevaEspecial);
+    nuevaEspecial.getElementsByTagName("button")[0].addEventListener("click",() => agregarAlCarrito(producto))
+  });
+}
+crearTarjetasProductosInicio(especiales);
