6f654c353bd2f8abef63da96ad086d6281092b97
3 <samuelgtz025@gmail.com>
2023-11-25 17:47:54 -0600

    pantalla ofertas

---
+++ docs/js/indexOfertas.js
@@ -0,0 +1,17 @@
+const contenedorTarjetas = document.getElementById("productos-container");
+
+/** Crea las tarjetas de productos teniendo en cuenta la lista en bicicletas.js */
+function crearTarjetasProductosInicio(productos){
+  productos.forEach(producto => {
+    const nuevaOferta = document.createElement("div");
+    nuevaOferta.classList = "tarjeta-producto"
+    nuevaOferta.innerHTML = `
+    <img src="./img/productos/${producto.id}.webp" alt="Oferta 1">
+    <h3>${producto.nombre}</h3>
+    <p class="precio">$${producto.precio}</p>
+    <button>Agregar al carrito</button>`
+    contenedorTarjetas.appendChild(nuevaOferta);
+    nuevaOferta.getElementsByTagName("button")[0].addEventListener("click",() => agregarAlCarrito(producto))
+  });
+}
+crearTarjetasProductosInicio(ofertas);
