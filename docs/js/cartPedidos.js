22324e8f9927199f199930ad7137116b81655d1a
3 <samuelgtz025@gmail.com>
2023-11-25 18:01:52 -0600

    pantalla pedidos

---
+++ docs/js/cartPedidos.js
@@ -0,0 +1,80 @@
+const contenedorTarjetas = document.getElementById("cart-container");
+const cantidadElement = document.getElementById("cantidad");
+const precioElement = document.getElementById("precio");
+const carritoVacioElement = document.getElementById("carrito-vacio");
+const totalesContainer = document.getElementById("totales");
+
+/** Crea las tarjetas de productos teniendo en cuenta lo guardado en localstorage */
+function crearTarjetasProductosCarrito() {
+  contenedorTarjetas.innerHTML = "";
+  const productos = JSON.parse(localStorage.getItem("pedidos"));
+  if (productos && productos.length > 0) {
+    productos.forEach((producto) => {
+      const nuevaPedido = document.createElement("div");
+      nuevaPedido.classList = "tarjeta-producto";
+      nuevaPedido.innerHTML = `
+    <img src="./img/productos/${producto.id}.webp" alt="pedidos 1">
+    <h3>${producto.nombre}</h3>
+    <span>$${producto.precio}</span>
+    <div>
+    <button>-</button>
+    <span class="cantidad">${producto.cantidad}</span>
+    <button>+</button>
+    </div>
+    `;
+      contenedorTarjetas.appendChild(nuevaPedido);
+      nuevaPedido
+        .getElementsByTagName("button")[0]
+        .addEventListener("click", (e) => {
+          const cantidadElement = e.target.parentElement.getElementsByClassName("cantidad")[0];
+          cantidadElement.innerText = restarAlCarrito(producto);
+          crearTarjetasProductosCarrito();
+          actualizarTotales();
+        });
+      nuevaPedido
+        .getElementsByTagName("button")[1]
+        .addEventListener("click", (e) => {
+          const cantidadElement = e.target.parentElement.getElementsByClassName("cantidad")[0];
+          cantidadElement.innerText = agregarAlCarrito(producto);
+          actualizarTotales();
+        });
+    });
+  }
+  revisarMensajeVacio();
+  actualizarTotales();
+  actualizarNumeroCarrito();
+}
+
+crearTarjetasProductosCarrito();
+
+/** Actualiza el total de precio y unidades de la página del carrito */
+function actualizarTotales() {
+  const productos = JSON.parse(localStorage.getItem("pedidos"));
+  let cantidad = 0;
+  let precio = 0;
+  if (productos && productos.length > 0) {
+    productos.forEach((producto) => {
+      cantidad += producto.cantidad;
+      precio += producto.precio * producto.cantidad;
+    });
+  }
+  cantidadElement.innerText = cantidad;
+  precioElement.innerText = precio;
+  if(precio === 0) {
+    reiniciarCarrito();
+    revisarMensajeVacio();
+  }
+}
+
+document.getElementById("reiniciar").addEventListener("click", () => {
+  contenedorTarjetas.innerHTML = "";
+  reiniciarCarrito();
+  revisarMensajeVacio();
+});
+
+/** Muestra o esconde el mensaje de que no hay nada en el carrito */
+function revisarMensajeVacio() {
+  const productos = JSON.parse(localStorage.getItem("pedidos"));
+  carritoVacioElement.classList.toggle("escondido", productos);
+  totalesContainer.classList.toggle("escondido", !productos);
+}
---
+++ docs/js/indexPedidos.js
@@ -0,0 +1,17 @@
+const contenedorTarjetas = document.getElementById("productos-container");
+
+/** Crea las tarjetas de productos teniendo en cuenta la lista en bicicletas.js */
+function crearTarjetasProductosInicio(productos){
+  productos.forEach(producto => {
+    const nuevaPedido = document.createElement("div");
+    nuevaPedido.classList = "tarjeta-producto"
+    nuevaPedido.innerHTML = `
+    <img src="./img/productos/${producto.id}.webp" alt="Pedidos 1">
+    <h3>${producto.nombre}</h3>
+    <p class="precio">$${producto.precio}</p>
+    <button>Agregar al carrito</button>`
+    contenedorTarjetas.appendChild(nuevaPedido);
+    nuevaPedido.getElementsByTagName("button")[0].addEventListener("click",() => agregarAlCarrito(producto))
+  });
+}
+crearTarjetasProductosInicio(pedidos);
